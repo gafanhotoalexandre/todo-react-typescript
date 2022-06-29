e589ccb808fdcdb76ac55fe967723075258bca9f
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2022-06-28 22:51:10 -0300

    estilizacao do header

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,12 @@ import * as C from './App.styles';
 export default function App() {
   return (
     <C.Container>
-      ...
+      <C.Area>
+        <C.Header>Lista de Tarefas com
+          <span>TypeScript</span>
+        </C.Header>
+
+      </C.Area>
     </C.Container>
   );
 }
